e675a1ac9ce36b63331b82a266232485b7623d46
Matt Vermont <matthew.vermont@yahoo.com>
2021-10-11 15:41:15 -0400

    first steps of game flow finished

--- src/main/scala/example/main.scala
+++ src/main/scala/example/main.scala
@@ -1,6 +1,7 @@
 package example
 
 import scala.io._
+import scala.collection._
 
 /**
   * The goal of the game will be to make it home safely after waking up in a dark 
@@ -16,12 +17,24 @@ object Main {
         bufferedSource.close()
 
         var textLine: Int = 0
-        printText(textLine)
-        textLine += 1
+        var deadLine: Int = 999 //will set deadLine when story is finished
 
-        var answer = StdIn.readLine()
-        println(answer)
-        print(textLine)
+        while(textLine != deadLine){
+            //Print description 
+            printText(textLine)
+
+            //Inform player of options
+            printOptions(textLine)
+
+            //Once answer is chosen, read the result
+            //return new textLine and set it
+            var answer = StdIn.readLine()
+            //println(answer)
+            var newLine: Int = printResults(textLine, answer.toInt)
+            textLine = newLine
+        }
+
+        println("You are now dead")
 
     }
 
@@ -37,7 +50,52 @@ object Main {
             case 0 => println("Intro to Game")
             case 1 => println("")
         }
+    }
 
+    /**
+      * After the choice is made by the user, check to see what happens
+      * as a result of those choices
+      *
+      * @param textLine
+      * @return
+      */
+    def checkOptions(textLine: Int): List[String] = {
+        //read tab from mongoDB
+        //split by , returning a list (or tuple) of results
+        List("a", "b")
+    }
 
+    def printOptions(textLine: Int) {
+        var options = checkOptions(textLine)
+        println("Would you like to: (Press 1 or 2)")
+        println(options(0))
+        println(options(1))
+    }
+
+    /**
+      * This will pull the result from mongoDB using what line we are one
+      * and the answer that the user gave
+      *
+      * @param textLine
+      * @param answer
+      * @return
+      */
+    def getResults(textLine: Int, answer: Int): String = {
+        "This is a test result,999"
+    }
+
+    /**
+      * This will print out the result of what happened in the story and
+      * return the new textLine that corresponds to that event
+      *
+      * @param textLine
+      * @param answer
+      * @return
+      */
+    def printResults(textLine: Int, answer: Int): Int = {
+        var results: String = getResults(textLine, answer)
+        val res = results.split(",")
+        println(res(0))
+        res(1).toInt
     }
 }
